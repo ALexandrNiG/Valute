1916f65ee62ac4ba847a1fa482b15a3720d3a13c
AlexandrNiG <undeleting1@gmail.com>
2022-04-30 00:32:26 +0300

    1

--- basics_2_lesson_PRO/task_6.py
+++ basics_2_lesson_PRO/task_6.py
@@ -1,7 +1,10 @@
-
 a = str(254314)
 
+h = 0
 i = 0
 while i < 6:
-    print(a[i], sep=" , ")
+    b = int(a[i])
+    h += b
     i += 1
+print("Сумма цифр числа - ", h, end='(')
+print(a[0], a[1], a[2], a[3], a[4], a[5], sep="+", end=')')
