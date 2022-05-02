82ed94a9c4a26b6c3eba1eb6d6e7ce762a7f0519
AlexandrNiG <undeleting1@gmail.com>
2022-05-02 04:20:40 +0300

    1

---
+++ черновик/all.py
@@ -0,0 +1,2 @@
+c = [c +'_3' for c in 'dfgh']
+print(c)
--- черновик/разделитель.py
+++ черновик/разделитель.py
@@ -1,3 +1,13 @@
 s0 = "lamp, bag, mirror"
 s = s0.split(", ") # s is a list: ["lamp", "bag", "mirror"]
 print(s)
+
+print('Hello!')
+print('Hello!', 'Student!', 123)
+print('Hello!', 'Student!', 123, sep = 'xxx')
+print('Hello!', 'Student!', 123, end = 'yyy')
+print()
+print("Alexandr \"Hi\" Nikolaevich")
+
+c = [c +'_3' for c in 'dfgh']
+print(c)
